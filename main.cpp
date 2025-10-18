0e1247d429f8769c51b365d9dca91dd0575fecdc
KoleSchramm <kole.schramm@csuglobal.edu>
2025-10-18 04:05:01 -0500

    Second Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 int main() {
+    string string1;
+    string string2;
 
+    for (int i = 0; i<3; i++) {
+        cout << "Input string #1" << endl;
+        cin >> string1;
+
+        cout << "Input string #2" << endl;
+        cin >> string2;
+
+        string1+=string2;
+
+        cout << "Output: " << string1 << endl;
+        cout << endl;
+    }
     return 0;
 }
